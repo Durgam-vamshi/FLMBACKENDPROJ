dd0295eef669985c71ff4dcecfac2ec168262e70
DeveloperSyamKumar <gorlisyam988@gmail.com>
2025-11-11 12:31:26 +0530

    Re-add project with latest updates

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/companies", companyRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
